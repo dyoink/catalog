85e04724cb11bb2f43464bdda86215c4f1734cea
Đức Trần <tranchiduc51003@gmail.com>
2026-04-29 00:50:10 +0700

    fix bug theme

--- src/AquaCMS/obj/Debug/net10.0/AquaCMS.AssemblyInfo.cs
+++ src/AquaCMS/obj/Debug/net10.0/AquaCMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaCMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e5ec0dda6152c4a563d470b5778cdcce015386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a8b51a85e7d04a36def99798cffca388a692d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaCMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaCMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
