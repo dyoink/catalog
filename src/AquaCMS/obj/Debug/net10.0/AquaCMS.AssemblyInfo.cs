1fa0bd541ca1349f18a41f12a918538e39b7cd4d
tcd <tranchiduc51003@gmail.com>
2026-05-05 10:09:47 +0700

    fix ui

--- src/AquaCMS/obj/Debug/net10.0/AquaCMS.AssemblyInfo.cs
+++ src/AquaCMS/obj/Debug/net10.0/AquaCMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaCMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a72d98e6f38bfc92dabb83930d30670f21eec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a03e393918896d6ffbfa380d71648ad4278b9ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaCMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaCMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
